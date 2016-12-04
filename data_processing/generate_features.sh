e2b2dc7b32ff41c2b48e5b62a6604223e552dca6
Edward Williams <edward_c_williams@brown.edu>
2016-12-03 22:28:00 -0500

    replacing GNU parallel with for loop

--- data_processing/generate_features.sh
+++ data_processing/generate_features.sh
@@ -4,4 +4,9 @@
 pdbfolder=../../data/raw_pdb
 outfolder=../../data/summed_suffstats
 
-parallel --jobs 2 --eta python feat_from_pdb.py -pdb {1} -o $outfolder ::: $pdbfolder/*.pdb
+#parallel --jobs 2 --eta python feat_from_pdb.py -pdb {1} -o $outfolder ::: $pdbfolder/*.pdb
+
+#using for loop instead
+for file in $pdbfolder/*; do	
+	python feat_from_pdb.py -pdb $file -o $outfolder
+done

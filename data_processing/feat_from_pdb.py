1795e8571b78e1d278600206944db8cbb0afc718
Roshan Rao <roshan_rao@brown.edu>
2016-12-03 23:35:39 -0500

    changed dtype to int

--- data_processing/feat_from_pdb.py
+++ data_processing/feat_from_pdb.py
@@ -85,7 +85,7 @@ def features_from_pdb(filename, outfolder):
     n_aa_combinations = len(AMINO_ACIDS)*(len(AMINO_ACIDS) + 1)/2
     seq_dist_idx = n_aa_combinations
     seq_len_idx = n_aa_combinations + 1
-    features = np.zeros(n_aa_combinations + 1 + 1) # 1 for distance, 1 for seqlength
+    features = np.zeros(n_aa_combinations + 1 + 1, dtype=int) # 1 for distance, 1 for seqlength
     features[seq_len_idx] = len(sequence)
 
     true_example = np.zeros((len(sequence)*(len(sequence) + 1)/2, 1))
@@ -107,10 +107,10 @@ def features_from_pdb(filename, outfolder):
         if dist < 10.0: #angstroms
             edges.add(tuple(sorted((int(seq_num1), int(seq_num2)))))
             true_example[i] = 1
-
+    num_edges = len(edges)
     edge_density = get_three_factor_stats(edges, len(sequence))
 
-    suff_stats_protein = np.concatenate((features, np.array([num_edges]), edge_density))
+    suff_stats_protein = np.concatenate((features, np.array([num_edges], dtype=int), edge_density))
 
 
     #convert to string, save file

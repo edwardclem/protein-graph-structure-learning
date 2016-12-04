ea21b651c8a9ecbe92a6df0a25514061ba136f49
Roshan Rao <roshan_rao@brown.edu>
2016-12-03 22:35:42 -0500

    changed directories

--- data_processing/generate_features.sh
+++ data_processing/generate_features.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #produces a graph file from all PDB files in the given folder
-pdbfolder=../../data/raw_pdb
-outfolder=../../data/summed_suffstats
+pdbfolder=/data/protein_contact_prediction/data/raw_pdb
+outfolder=../data/summed_suffstats
 
 #parallel --jobs 2 --eta python feat_from_pdb.py -pdb {1} -o $outfolder ::: $pdbfolder/*.pdb
 

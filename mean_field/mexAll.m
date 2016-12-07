07a6cebd1bc753c5e79051283a424ccb4abedbad
Roshan Rao <roshan_rao@brown.edu>
2016-12-07 15:27:19 -0500

    added mexAll file

---
+++ mean_field/mexAll.m
@@ -0,0 +1,3 @@
+fprintf('Compiling mean_field CRF files...\n');
+mex -outdir ./build ./mex/calcF.cpp CXXFLAGS="\$CXXFLAGS -std=c++11"
+mex -outdir ./build ./mex/calc_muhat.cpp CXXFLAGS="\$CXXFLAGS -std=c++11"

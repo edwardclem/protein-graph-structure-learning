09f8f96d5f855e8987fb4a48c20871dc4f7bb032
Edward Williams <edward_c_williams@brown.edu>
2016-12-08 22:56:21 -0500

    mexAll multithreading

--- mean_field/mexAll.m
+++ mean_field/mexAll.m
@@ -1,3 +1,4 @@
 fprintf('Compiling mean_field CRF files...\n');
 mex -outdir ./build ./mex/calcF.cpp CXXFLAGS="\$CXXFLAGS -std=c++11"
 mex -outdir ./build ./mex/calc_muhat.cpp CXXFLAGS="\$CXXFLAGS -std=c++11"
+mex -outdir ./build ./mex/calc_muhat_parallel.cpp CXXFLAGS="\$CXXFLAGS -std=c++11"

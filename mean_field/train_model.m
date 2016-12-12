fce0e1fdda7105802799913a70f8bcf6c30933ae
Edward Williams <edward_c_williams@brown.edu>
2016-12-11 19:44:16 -0500

    added cond to mexAll

--- mean_field/train_model.m
+++ mean_field/train_model.m
@@ -44,5 +44,5 @@ all_mus = vertcat(muhat{1:end});
 all_gt = vertcat(gt{1:end});
 
 [X, Y, T, AUC] = perfcurve(all_gt, all_mus, 1);
-save(outfile, 'X', 'Y', 'T', 'AUC', 'thetaML',);
+save(outfile, 'X', 'Y', 'T', 'AUC', 'thetaML');
 end
